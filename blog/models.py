44ae8481ee1e4378bd630c1f239526fc687d83ab
erkinov-khumoyun <erkinovkhumoyun@gmail.com>
2022-12-03 23:58:14 +0500

    now this codes get render very difficul for me especially list,detail.html

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.utils import timezone
 from  django.contrib.auth.models import User
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
+
+
 class Post(models.Model):
 
 
@@ -19,6 +28,9 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
+    object = models.Manager()
+    published = PublishedManager()
+
 
     class Meta:
         ordering = ['-publish']
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Post
+from django.views.generic import ListView
 
 # Create your views here.
+
+def post_list(request):
+    posts = Post.published.all() #This codes bring all of elements from Database
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts':posts})
+
+
+
+
+
+def post_detail(reqeust,id):
+    post = get_object_or_404(Post, #This shortcut
+                             id=id,
+                             status = Post.Status.PUBLISHED)
+    return render(reqeust,'blog/post/detail.html',
+                  {'post':post})
