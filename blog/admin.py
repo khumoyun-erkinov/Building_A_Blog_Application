64d8f52984d930bd54c8a79773ed5d6f482f4bee
erkinov-khumoyun <erkinovkhumoyun@gmail.com>
2022-12-03 16:58:22 +0500

    this code is very usefull and helpfull for beginner programmers

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,10 @@ from .models import Post
 # admin.site.register(Post)
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title','slug','author','publish','statue']
+    list_display = ['title','slug','author','publish','status']
+    list_filter = ['status', 'created', 'publish', 'author']
+    search_fields = ['title', 'body'] #This codes displays title and body`s searching
+    prepopulated_fields = {'slug': ('title',)} # This shows us Django administraion part of action
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish' # This shows us when code is written,create
+    ordering = ['status', 'publish']
