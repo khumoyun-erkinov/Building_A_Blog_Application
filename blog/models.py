d73c61506af709aed56683a7b8f6e0447b158751
erkinov-khumoyun <erkinovkhumoyun@gmail.com>
2022-12-05 00:54:18 +0500

    Today I create codes for pagination.html.it helps us to shows sequence of  pages,such as page=1 , page=2

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
-from  django.contrib.auth.models import User
+from django.contrib.auth.models import User
+from django.urls import reverse
+
 
 
 class PublishedManager(models.Manager):
@@ -18,7 +20,8 @@ class Post(models.Model):
         PUBLISHED = 'PB','Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')
     author = models.ForeignKey(User,on_delete=models.CASCADE,
                                related_name='blog_posts')
     body = models.TextField()
@@ -28,8 +31,9 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
-    object = models.Manager()
-    published = PublishedManager()
+
+
+
 
 
     class Meta:
@@ -41,3 +45,15 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    object = models.Manager()
+    published = PublishedManager()
+
+
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,45 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Post
-from django.views.generic import ListView
+from django.core.paginator import Paginator
+
+
 
 # Create your views here.
 
+# def post_list(request):
+#     posts = Post.published.all() #This codes bring all of elements from Database
+#     return render(request,
+#                   'blog/post/list.html',
+#                   {'posts':posts})
+
+
 def post_list(request):
-    posts = Post.published.all() #This codes bring all of elements from Database
-    return render(request,
-                  'blog/post/list.html',
+    post_list = Post.published.all()
+    pagintor = Paginator(post_list,3)
+    page_number = request.GET.get('page',1)
+    posts = pagintor.page(page_number)
+    return render(request,'blog/post/list.html',
                   {'posts':posts})
 
 
 
 
 
-def post_detail(reqeust,id):
-    post = get_object_or_404(Post, #This shortcut
-                             id=id,
-                             status = Post.Status.PUBLISHED)
-    return render(reqeust,'blog/post/detail.html',
+# def post_detail(reqeust,id):
+#     post = get_object_or_404(Post, #This shortcut
+#                              id=id,
+#                              status = Post.Status.PUBLISHED) ## This code for showing  id
+#     return render(reqeust,'blog/post/detail.html',
+#                   {'post':post})
+
+
+def post_detail(request,year,month,day,post):
+    post = get_object_or_404(Post,
+                             status = Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request,
+                  'blog/post/detail.html',
                   {'post':post})
